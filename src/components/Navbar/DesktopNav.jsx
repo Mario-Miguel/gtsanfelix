9669e88960e472889385aba80ec602d2299b00c3
Mario-Miguel <mariomig5@gmail.com>
2023-05-24 23:10:33 +0200

    removed dummy routes

--- src/components/Navbar/DesktopNav.jsx
+++ src/components/Navbar/DesktopNav.jsx
@@ -1,47 +1,6 @@
 import { Box, Link, Popover, PopoverContent, PopoverTrigger, Stack, useColorModeValue } from "@chakra-ui/react";
 import DesktopSubNav from "./DesktopSubNav";
 
-const NAV_ITEMS = [
-	{
-		label: "Inspiration",
-		children: [
-			{
-				label: "Explore Design Work",
-				subLabel: "Trending Design to inspire you",
-				href: "#",
-			},
-			{
-				label: "New & Noteworthy",
-				subLabel: "Up-and-coming Designers",
-				href: "#",
-			},
-		],
-	},
-	{
-		label: "Find Work",
-		children: [
-			{
-				label: "Job Board",
-				subLabel: "Find your dream design job",
-				href: "#",
-			},
-			{
-				label: "Freelance Projects",
-				subLabel: "An exclusive list for contract work",
-				href: "#",
-			},
-		],
-	},
-	{
-		label: "Learn Design",
-		href: "#",
-	},
-	{
-		label: "Hire Designers",
-		href: "#",
-	},
-];
-
 const DesktopNav = ({ navItems }) => {
 	const linkColor = useColorModeValue("gray.600", "gray.200");
 	const linkHoverColor = useColorModeValue("gray.800", "white");
